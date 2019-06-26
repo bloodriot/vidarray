c6f1f92ead4f5e0d82941577856786bda35a041f
bloodriot <jasoncreswell@gmail.com>
2019-06-25 20:18:51 -0400

    Added some additional endpoints. Haven't worked on this in awhile so I don't remember what was going on.

--- src/main/web/webpack.config.js
+++ src/main/web/webpack.config.js
@@ -1,52 +1,47 @@
-import webpack from 'webpack';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import LiveReloadPlugin from 'webpack-livereload-plugin'
-import path from 'path'
 
-const config = {
-    entry: './client/js/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, "client/")
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'client/index.html'
-        }),
-        new LiveReloadPlugin()
-    ],
-    module: {
-        rules: [{
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: ['babel-loader']
-            },
-            {
-                use: ['style-loader', 'css-loader'],
-                test: /\.css$/
-            },
-            {
-                test: /\.scss$/,
-                use: [{
-                    loader: "style-loader"
-                }, {
-                    loader: "css-loader",
-                    options: {
-                        sourceMap: true
-                    }
-                }, {
-                    loader: "sass-loader",
-                    options: {
-                        sourceMap: true
-                    }
-                }]
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['*', '.js', '.jsx']
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const outputDirectory = 'dist';
+
+module.exports = {
+  entry: ['babel-polyfill', './client/index.jsx'],
+  output: {
+    path: path.join(__dirname, outputDirectory),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
+      }
+    ]
+  },
+  devServer: {
+    port: 3000,
+    open: true,
+    proxy: {
+      '/api': 'http://localhost:9098'
     }
+  },
+  plugins: [
+    new CleanWebpackPlugin([outputDirectory]),
+    new HtmlWebpackPlugin({
+      template: './client/index.html',
+      favicon: './client/favicon.ico'
+    })
+  ]
 };
-
-// Exports
-module.exports = config;
